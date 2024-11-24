dcd3a5d9537c2df966ac466feae42c216071c5cb
Vendula Konečná <VendaKonecna@gmail.com>
2024-11-24 23:02:39 +0100

    oprava

--- script.js
+++ script.js
@@ -2,28 +2,25 @@ const justFood = pocetLidi => {
     const cena = pocetLidi * 100
     return `Catering od Just Food pro ${pocetLidi} lidí za ${cena} Kč.`
 }
-pocetLidi = 10
-console.log(justFood(pocetLidi))
+
 
 const yourMama = pocetLidi => {
     const cena = pocetLidi * 200
     return `Catering od Your Mama pro ${pocetLidi} lidí za ${cena} Kč.`
 }
-pocetLidi = 10
-console.log(yourMama(pocetLidi))
+
 
 const flavourHaven = pocetLidi => {
     const cena = pocetLidi * 500
     return `Catering od Flavour Haven pro ${pocetLidi} lidí za ${cena} Kč.`
 }
-pocetLidi = 100
-console.log(flavourHaven(pocetLidi))
-
 
 
 const createEvent = (name, numberPeople, cateringFunction) => {
-    const flavourHaven = cateringFunction => {
-
-    }
-    return ``
+    return `Událost ${name} s ${cateringFunction(numberPeople)}`
 }
+ 
+document.body.innerHTML += `<p>${justFood(10)}</p>`
+document.body.innerHTML += `<p>${yourMama(10)}</p>`
+document.body.innerHTML += `<p>${flavourHaven(10)}</p>`
+document.body.innerHTML += `<p>${createEvent("Inaugurace prezidenta", 100, flavourHaven)}</p>`
